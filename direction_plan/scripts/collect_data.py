a22ea8422c083b125256daaa3d2b19be32ce6893
greatmanokonkwo <greatmanokonkwo@gmail.com>
2020-12-01 10:24:27 -0500

    Rewrote data collection script to be more precise when retreiving angle data

--- direction_plan/scripts/collect_data.py
+++ direction_plan/scripts/collect_data.py
@@ -20,18 +20,16 @@ def initialize_devices(preview=True):
 		time.sleep(5)
 		picam.stop_preview()
 
-def get_halt_signal():
+def get_halt_signal(accel):
 	# The halt signal will be ON if a large negative value in the x-direction is calculated
 
-	accel_x = mpu.get_accel_data()['x']
-		
-	if abs(accel_x) > C && accel_x%2!=0:
+	if abs(accel) > C && accel%2!=0:
 		return 1
 	else: 
 		return 0
 	
 # Data collection process	
-def data_collection(fps, mins):
+def data_collection(mins):
 	
 	# Get the ID of the last processed data sample
 	with open("../../dataset/last.txt") as f:
@@ -41,19 +39,36 @@ def data_collection(fps, mins):
 	
 	angle_halt = open("../../dataset/angle_halt.txt", "a")	
 
+	max_angle = 0
+	max_accel = 0
+	
+	segment_start = time.time()
 	# Loop until specified minutes have elasped
 	while time.time() - start_time < mins*60:
-		count+=1
-	
-		# Capture and save image
-		picam.capture("../../dataset/images/"+count+".jpg")
-
 		angle = mpu.get_angle_data()['z']
-		halt = get_halt_signal()
+		accel = mpu.get_accel_data()['x']
+		
+		if abs(angle) > abs(max_angle):
+			max_angle = angle
+	
+		if abs(accel) > abs(max_accel):
+			max_accel = accel
+	
+		if time.time() - segment_start > 1:
+			count+=1
+			# Capture and save image
+			picam.capture("../../dataset/images/"+count+".jpg")
+
+			halt = get_halt_signal(max_accel)
 			
-		# write the calculated values of the angle and the halt signal to the angle_halt.txt file	
-		angle_halt.write(angle+" "+halt+"\n")		
-		time.sleep(1/fps)
+			# write the calculated values of the angle and the halt signal to the angle_halt.txt file	
+			print(max_angle+" "+max_accel+"\n")
+			angle_halt.write(max_angle+" "+max_accel+"\n")		
+
+			max_angle = 0
+			max_accel = 0
+
+			segment_start = time.time()	
 
 	angle_halt.close()
 
