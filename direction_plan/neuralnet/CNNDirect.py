6ad8cecc49b6cbcb7f539b78c1abdd246a971518
greatmanokonkwo <greatmanokonkwo@gmail.com>
2020-12-05 01:07:31 -0500

    Edited To-Do List on README.md

--- direction_plan/neuralnet/CNNDirect.py
+++ direction_plan/neuralnet/CNNDirect.py
@@ -2,11 +2,11 @@
 This is an implementation of the AlexNet convolutional neural network. 
 It was 2 output neurons, the halt signal and the angular displacement in time
 """
+import torch
+import torch.nn as nn
 
-class CNNDirect:
+class CNNDirect(nn.Module):
 	def __init__(self):
 	
-	def train(self):
-	
-	def predict(self):
+	def forward(self):
 	
