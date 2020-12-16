4b96ae2639ce4d237513c9dbe6df26505f0dcd4f
greatmanokonkwo <greatmanokonkwo@gmail.com>
2020-12-16 15:21:20 -0500

    Updated mpu9250.py

--- direction_plan/devices/mpu9250.py
+++ direction_plan/devices/mpu9250.py
@@ -30,30 +30,30 @@ class MPU9250:
 	GYRO_OUT = 0x43
 
 	I2C_MST_DELAY_CTRL = 0x67
-	SIGNAL_PATH_RESET  = 0x68
-	MOT_DETECT_CTRL    = 0x69
-	USER_CTRL          = 0x6A
-	PWR_MGMT_1         = 0x6B
-	PWR_MGMT_2         = 0x6C
-	FIFO_R_W           = 0x74
-	WHO_AM_I           = 0x75
+	SIGNAL_PATH_RESET = 0x68
+	MOT_DETECT_CTRL = 0x69
+	USER_CTRL = 0x6A
+	PWR_MGMT_1 = 0x6B
+	PWR_MGMT_2 = 0x6C
+	FIFO_R_W = 0x74
+	WHO_AM_I = 0x75
 
 	## Gyro Full Scale Select 250dps
-	GFS_250  = 0x00
+	GFS_250 = 0x00
 	## Gyro Full Scale Select 500dps
-	GFS_500  = 0x01
+	GFS_500 = 0x01
 	## Gyro Full Scale Select 1000dps
 	GFS_1000 = 0x02
 	## Gyro Full Scale Select 2000dps
 	GFS_2000 = 0x03
 	## Accel Full Scale Select 2G
-	AFS_2G   = 0x00
+	AFS_2G = 0x00
 	## Accel Full Scale Select 4G
-	AFS_4G   = 0x01
+	AFS_4G = 0x01
 	## Accel Full Scale Select 8G
-	AFS_8G   = 0x02
+	AFS_8G = 0x02
 	## Accel Full Scale Select 16G
-	AFS_16G  = 0x03
+	AFS_16G = 0x03
 
 	## Gyro error
 	GYRO_ERROR_X = 0
@@ -66,20 +66,20 @@ class MPU9250:
 	ACCEL_ERROR_Z = 0
 
 	# AK8963 Register Addresses
-	AK8963_ST1        = 0x02
+	AK8963_ST1 = 0x02
 	AK8963_MAGNET_OUT = 0x03
-	AK8963_CNTL1      = 0x0A
-	AK8963_CNTL2      = 0x0B
-	AK8963_ASAX       = 0x10
+	AK8963_CNTL1 = 0x0A
+	AK8963_CNTL2 = 0x0B
+	AK8963_ASAX = 0x10
 
 	# CNTL1 Mode select
 	## Power down mode
-	AK8963_MODE_DOWN   = 0x00
+	AK8963_MODE_DOWN = 0x00
 	## One shot data output
-	AK8963_MODE_ONE    = 0x01
+	AK8963_MODE_ONE = 0x01
 
 	## Continous data output 8Hz
-	AK8963_MODE_C8HZ   = 0x02
+	AK8963_MODE_C8HZ = 0x02
 	## Continous data output 100Hz
 	AK8963_MODE_C100HZ = 0x06
 
@@ -185,9 +185,9 @@ class MPU9250:
 		time.sleep(0.01)
 
 	## brief Check data ready
-    #  @param [in] self The object pointer.
-    #  @retval true data is ready
-    #  @retval false data is not ready
+	#  @param [in] self The object pointer.
+	#  @retval true data is ready
+	#  @retval false data is not ready
 	def checkDataReady(self):
 		drdy = bus.read_byte_data(self.address, self.INT_STATUS)
 		if drdy & 0x01:
