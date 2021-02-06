5c7897e421a4ae2dbfb6df7d7a0054c47166a37c
Greatman Okonkwo <greatmanokonkwo@gmail.com>
2021-02-06 05:24:02 -0500

    Update collect_data.py

--- guide/collect_data.py
+++ guide/collect_data.py
@@ -1,4 +1,45 @@
-# Add vics directory to sys path to have access to devices module
+"""
+BEFORE RUNNING DATA_COLLECTION SCRIPT MAKE SURE THE MPU9250 HAS BEEN PROPERLY CALIBERATED AND PRODUCEDS ACCURATE YAW VALUES
+
+SPEED CONSIDERATIONS:
+- Walk at an average pace of 0.5 m/s or anything around there. 
+- If you need to walker faster/slower then you would need to change to update time of the collector
+- The collector runs at an update time of approx 1 second (collects image and direction pairs)
+- Modify the UPDATE_TIME constant just on the top of the file. The wearter can test out direction UPDATE_TIME values for optimal usage
+
+ROTATIONS:
+- Rotations are calculated as the maximum displacement angle within the UPDATE_TIME span
+- That being said the wearer should be certain of the direction they want to go. Make dilberate turns
+- DON'T TURN MORE THAN 90 DEGREES IN EACH DIRECTION! The system only considers angles within [-101.25, 101.25] degrees (202.25 degrees) 
+- Rotations should also be done in the window of the update time 
+
+STOPPING:
+- The stopping detector works by keep track of the bouncing motion on the z-axis. 
+- Since while walking the user moves up and down and algorithm knows the user has stopped walking when the up and down motion stops
+- The up and down motion is detected using the accelerometer sensors z-axis
+- At rest the value of the accelerometer's z-axis is -9.8 meters, representing the acceleration due to gravity
+- The algorithm calculates the accelerometer values over the spand of UPDATE_TIME and saves the maximum value over that interval
+- Then, if the difference between the maximum acceleration value and the rest value is greater than some threshold value THRESH, it is concluding that user is walking
+- The value of THRESH can be tweaked to match the walking style of the user.
+
+TESTING:
+- For testing the angle, halt and final direction class values are printed out to the standard output
+- Looking at these values can help tweak some of the values like UPDATE_TIME and THRESH
+- To test yaw estimation go to devices/test_mpu.py
+
+STOPPING CONDITIONS:
+- Stop when a human being is in front of you
+- Stop in front of a crosswalk
+- Stop when there is a closed door in front of you in a closed room, open in the door and continue. Whiling opeing door, try not to make a lot of movements
+- Stop when stairs have been encountered. Try to get top and bottom views of stairs in data. Same goes for escalators if any encountered. Highly doubt though
+
+WHAT TO DO:
+- Walk down multiple routes like you would normally do
+- Try to capture edge cases such as environments with glass doors, stairs, 
+
+
+
+""" 
 import os
 os.sys.path.append("..")
 
@@ -9,6 +50,11 @@ from imusensor.MPU9250 import MPU9250
 from imusensor.filters import madgwick
 
 from devices.picam import picam
+
+# UPDATE TIMER vARAIBLE
+UPDATE_TIME = 1
+MIDLINE = -9.8
+THRESH = 0.6
 
 # MPU9250 IMU sensor and sensorfusion algorithm
 sensorfusion = madgwick.Madgwick()
@@ -130,16 +176,19 @@ def data_collection(mins, path):
 			max_angle = angle
 				
 		# Save the image as well as the motion in format direct_class/id_angle.jpg
-		if newTime - interval_start >= 1:
+		if newTime - interval_start >= UPDATE_TIME:
 
-			if (max_accel - midline) < thresh:
+			halt = 0
+			if (max_accel - MIDLINE) < THRESH:
 				direct_class = 9 # Halt was detected
+				halt = 1
 			else:
 				direct_class = get_direction_class(max_angle)
 
 			if direct_class != -1:
 				cam.save_image(path=(path+"/"+str(direct_class)+"/"+str(count)+"_"+str(max_angle)+".jpg"), img=img_)
-				print(max_angle, max_accel, direct_class)
+				# Test out the angle, stop and direction class values. For max_accel if you are walking it should be 0 and if you stop it should be 1
+				print(max_angle, halt, direct_class)
 			else:
 				print("Invalid movement direction")
 
