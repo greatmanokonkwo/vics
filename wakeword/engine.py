6846a530337ddf76c1d58760a5bed0781f220cbb
greatmanokonkwo <greatmanokonkwo@gmail.com>
2021-03-22 16:54:39 -0400

    ?

--- wakeword/engine.py
+++ wakeword/engine.py
@@ -1,4 +1,7 @@
 """the interface to interact with wakeword model"""
+import os.sys
+sys.path.append("..")
+
 import pyaudio
 import threading
 import time
@@ -10,31 +13,7 @@ import numpy as np
 from neuralnet.dataset import get_featurizer
 from threading import Event
 
-
-class Listener:
-
-    def __init__(self, sample_rate=8000, record_seconds=2):
-        self.chunk = 1024
-        self.sample_rate = sample_rate
-        self.record_seconds = record_seconds
-        self.p = pyaudio.PyAudio()
-        self.stream = self.p.open(format=pyaudio.paInt16,
-                        channels=1,
-                        rate=self.sample_rate,
-                        input=True,
-                        output=True,
-                        frames_per_buffer=self.chunk)
-
-    def listen(self, queue):
-        while True:
-            data = self.stream.read(self.chunk , exception_on_overflow=False)
-            queue.append(data)
-            time.sleep(0.01)
-
-    def run(self, queue):
-        thread = threading.Thread(target=self.listen, args=(queue,), daemon=True)
-        thread.start()
-        print("\nWake Word Engine is now listening... \n")
+from devices.listener import Listener
 
 
 class WakeWordEngine:
